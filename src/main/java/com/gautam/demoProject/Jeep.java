b9d9d541f97bdcb7db3ecb605d7481cde399a639
Gautam Raj <gautamraj996@gmail.com>
2024-01-17 12:45:37 +0530

    annotation based configuration done

--- src/main/java/com/gautam/demoProject/Jeep.java
+++ src/main/java/com/gautam/demoProject/Jeep.java
@@ -1,5 +1,8 @@
 package com.gautam.demoProject;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Jeep implements Vehicle{
 	public void drive() {
 		System.out.println("Bhaag rahi hai");
