0c1110010d7620a32424ce97cab17a1cd745c517
Gautam Raj <gautamraj996@gmail.com>
2024-01-17 12:58:11 +0530

    bean property done

--- src/main/java/com/gautam/demoProject/App.java
+++ src/main/java/com/gautam/demoProject/App.java
@@ -11,8 +11,11 @@ public class App
     public static void main( String[] args ) {
     	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     	
-    	Vehicle obj = (Vehicle)context.getBean("jeep");
-    	obj.drive();
+//    	Vehicle obj = (Vehicle)context.getBean("jeep");
+//    	obj.drive();
+    	
+    	Tyre t = (Tyre)context.getBean("tyre");
+    	System.out.println(t);
     }
 
 }
